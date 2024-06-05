6c708cbbdfa8978425894f75678408d4e06714d2
claven06 <105958751+claven06@users.noreply.github.com>
2024-06-05 11:17:44 +0800

    Update PE.v

--- src/PE.v
+++ src/PE.v
@@ -1,31 +1,26 @@
-module PE (clk, rst_n, ifm_input0, ifm_input1, ifm_input2, ifm_input3, 
-            wgt_input0, wgt_input1, wgt_input2, wgt_input3, p_sum);
-
-input clk;
-input rst_n;
-input signed [7:0] ifm_input0;
-input signed [7:0] ifm_input1;
-input signed [7:0] ifm_input2;
-input signed [7:0] ifm_input3;
-
-input signed [7:0] wgt_input0;
-input signed [7:0] wgt_input1;
-input signed [7:0] wgt_input2;
-input signed [7:0] wgt_input3;
-
-output signed [24:0] p_sum;
+module PE (
+    input clk,
+    input rst_n,
+    input signed [7:0] ifm_input0,
+    input signed [7:0] ifm_input1,
+    input signed [7:0] ifm_input2,
+    input signed [7:0] ifm_input3,
+    input signed [7:0] wgt_input0,
+    input signed [7:0] wgt_input1,
+    input signed [7:0] wgt_input2,
+    input signed [7:0] wgt_input3,
+    output signed [24:0] p_sum
+);
 
 reg signed [15:0] product [3:0];
 reg signed [16:0] pp_sum [1:0];
 reg signed [24:0] p_sum;
 
-
 integer i;
 integer j;
 
-always @(posedge clk or negedge rst_n) 
-    if (~rst_n) 
-    begin
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin // reset all registers to 0 when rst_n is low
         for(i = 0; i < 4; i = i + 1) 
         begin
             product[i] <= 0;
@@ -37,8 +32,7 @@ always @(posedge clk or negedge rst_n)
         end
         p_sum <= 0;
     end
-    else
-    begin
+    else begin // multiply and accumulate to achieve partial sum
         product[0] <= ifm_input0 * wgt_input0;
         product[1] <= ifm_input1 * wgt_input1;
         product[2] <= ifm_input2 * wgt_input2;
@@ -48,6 +42,5 @@ always @(posedge clk or negedge rst_n)
         pp_sum[1] <= product[2] + product[3];
         p_sum <= pp_sum[0] + pp_sum[1];
     end
-
-
-endmodule //PE
+end
+endmodule
