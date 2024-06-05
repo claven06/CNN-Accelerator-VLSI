9794ce2fa55f8a1a7bd424f3040d4fc7416ee41f
claven06 <105958751+claven06@users.noreply.github.com>
2024-06-05 11:18:51 +0800

    Update WGT_BUF.v

--- src/WGT_BUF.v
+++ src/WGT_BUF.v
@@ -1,48 +1,43 @@
-module WGT_BUF (clk, rst_n, wgt_input, wgt_read, wgt_buf0, wgt_buf1, wgt_buf2, wgt_buf3);
-
-input clk;
-input rst_n;
-input signed [7:0] wgt_input;
-input wgt_read;
-output signed [7:0] wgt_buf0;
-output signed [7:0] wgt_buf1;
-output signed [7:0] wgt_buf2;
-output signed [7:0] wgt_buf3;
+module WGT_BUF (
+    input clk,
+    input rst_n,
+    input wgt_read,
+    input signed [7:0] wgt_input,
+    output signed [7:0] wgt_buf0,
+    output signed [7:0] wgt_buf1,
+    output signed [7:0] wgt_buf2,
+    output signed [7:0] wgt_buf3
+);
 
 reg signed [7:0] wgt_buf [3:0];
 
-
 integer i;
 
-always @(posedge clk or negedge rst_n) 
-    if (~rst_n) 
-    begin
-        for(i = 0; i < 4; i = i + 1) 
-        begin
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin // reset all buffers to 0 when rst_n is low
+        for(i = 0; i < 4; i = i + 1) begin
             wgt_buf[i] <= 0;
         end
     end
-    else
-    begin
-        if(wgt_read)
-        begin
+    else begin
+        if(wgt_read) begin // shift buffer from input when wgt_read is high
             wgt_buf[3] <= wgt_buf[2];
             wgt_buf[2] <= wgt_buf[1];
             wgt_buf[1] <= wgt_buf[0];
             wgt_buf[0] <= wgt_input;
         end
-        else
-        begin
+        else begin // keep buffer value same when wgt_read is low
             wgt_buf[3] <= wgt_buf[3];
             wgt_buf[2] <= wgt_buf[2];
             wgt_buf[1] <= wgt_buf[1];
             wgt_buf[0] <= wgt_buf[0];
         end
     end
+end
 
-    assign wgt_buf0 = wgt_buf[0];
-    assign wgt_buf1 = wgt_buf[1];
-    assign wgt_buf2 = wgt_buf[2];
-    assign wgt_buf3 = wgt_buf[3];
-
-endmodule 
+// connect buffer values to output ports
+assign wgt_buf0 = wgt_buf[0];
+assign wgt_buf1 = wgt_buf[1];
+assign wgt_buf2 = wgt_buf[2];
+assign wgt_buf3 = wgt_buf[3];
+endmodule
