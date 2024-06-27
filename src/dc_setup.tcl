7b9f64cfc70673d2a655e7a7222763d3b067cdec
claven06 <105958751+claven06@users.noreply.github.com>
2024-06-27 18:27:23 +0800

    Delete src/dc_setup.tcl

--- src/dc_setup.tcl
+++
@@ -1,4 +0,0 @@
-set search_path "$search_path . .. ../../lib/stdcell_lvt/db_nldm"
-set_app_var target_library "saed32lvt_ss0p75vn40c.db"
-set_app_var link_library "* saed32lvt_ss0p75vn40c.db"
-set_min_library saed32lvt_ss0p75vn40c.db -min_version saed32lvt_ff1p16vn40c.db
