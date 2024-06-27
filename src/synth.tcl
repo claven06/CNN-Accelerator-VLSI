847bb368f88a03bed6ea7bfd3729b1f3ca330273
claven06 <105958751+claven06@users.noreply.github.com>
2024-06-27 18:27:13 +0800

    Update synth.tcl

--- src/synth.tcl
+++ src/synth.tcl
@@ -4,7 +4,11 @@ set cache_write work
 set cache_read  work
 define_design_lib WORK -path work
 
-source dc_setup.tcl
+# Set the search and library paths
+set search_path "$search_path . .. ../../lib/stdcell_lvt/db_nldm"
+set_app_var target_library "saed32lvt_ss0p75vn40c.db"
+set_app_var link_library "* saed32lvt_ss0p75vn40c.db"
+set_min_library saed32lvt_ss0p75vn40c.db -min_version saed32lvt_ff1p16vn40c.db
 
 # Elaborate Design
 set DESIGN    "CONV_ACC"
