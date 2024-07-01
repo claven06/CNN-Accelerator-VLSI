4442706be1e9a4266b8bff00f5bace259576fe5f
claven06 <105958751+claven06@users.noreply.github.com>
2024-07-01 22:20:57 +0800

    Update full_run.sh

--- script/full_run.sh
+++ script/full_run.sh
@@ -3,9 +3,8 @@
 # Remove log file
 rm ./sim_run.log
 rm ./syn_run.log
-
-# Remove reports
-rm ./
+rm ./post_sim_run.log
 
 ./sim_run.sh > sim_run.log
 ./syn_run.sh > syn_run.log
+./post_sim_run.sh > post_sim_run.log
